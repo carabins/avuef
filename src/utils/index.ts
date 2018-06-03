d6b9191b7ed48c4dbc77804d45ba4be8602c2d83
gleba <dev@gleb.pw>
2018-06-03 16:40:10 +0300

    v0.0.8

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ export function pathTo(path, inObject) {
   let lo = inObject
   let wrong = false
   pa.forEach(p => {
-    if (lo != undefined && lo.hasOwnProperty(p)) {
+    if (lo != undefined && lo[p]) {
       lo = lo[p]
     } else {
       wrong = true
