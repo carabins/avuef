5600892909fc8559edfa7cce9660f4e79da7655b
gleba <dev@gleb.pw>
2018-06-15 07:27:21 +0300

    v0.0.23

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,58 @@
-export declare interface AVueAction {
-  launch(command: string, params?)
-  state: any
-  during: any
+import {AFlow} from "alak/dist/def/AFlow";
+
+export declare interface AVueActions {
+  /**
+   * Call action by path with argument,
+   * @param {string} actionPath
+   * @param args
+   * @returns {Promise<any>}
+   */
+  launch(actionPath: string, ...args): Promise<any> | any
+
+  /**
+   * Global store for nodes with params `state` in flow graph schema
+   */
+  state: { [flowName: string]: any }
+  /**
+   * Progress bollean state for any action by same path
+   */
+  during: { [actionPath: string]: boolean }
 }
+
+type AVueFlow = {
+  (s?): AFlow<typeof s>
+  [metaParam: string]: AFlow<any>
+}
+
 export declare interface IA {
-  f:any
-  on: (node: string, action: string)=>any
-  lazyOn: (node: string, action: string)=>any
-  lazyGet:any
-  get:any
+  f: AVueFlow
+  flow: AVueFlow
+  on: (node: string, action: string) => AFlow<any>
+  lazyOn: (node: string, action: string) => AFlow<any>
+  get: (action: string) => AFlow<any>
+  lazyGet: (action: string) => AFlow<any>
 }
-export declare const A:IA
+
+export declare const A: IA
+
+export type AFlowMutator<T> = T & FlowMutator
+export type FlowMutator = {
+  (s?): AFlow<typeof s>
+}
+
 export declare class AVue<T> {
-  a: AVueAction
-  f: T
+  a: AVueActions
+  f: AFlowMutator<T>
+
   constructor(schemaClass: T, actionModules: {
-    (a: AVueAction, f: T): {
+    (a, f: T): {
       [s: string]: Function
     }
   })
+}
+
+declare global {
+  interface Vue<T> {
+    $a: AVueActions
+  }
 }
