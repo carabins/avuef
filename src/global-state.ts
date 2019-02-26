3d1036668a3ea041439e1baf8ffbbdbbfa4d73cc
Gleb YP <dev@gleb.pw>
2019-02-26 20:47:26 +0300

    v0.2.23

--- src/global-state.ts
+++ src/global-state.ts
@@ -7,22 +7,23 @@ export function observableValue(v){
     Object.keys(v).forEach(k=>observable(v[k]))
   return v
 }
-
+const data = {
+  during: {},
+  state: {}
+}
 export const GlobalState = {
   init(vue){
     this.Vue = vue
-    observableValue(GlobalState.Vue)
+    observableValue(data)
   },
   Vue:{} as any,
-  data: {
-    during: {},
-    state: {}
-  },
+  data,
   setRun: (key, value) => {
     // let o = wm.$data.during
     // wm.$set(wm, 'during', Object.assign({}, o, {[key]: value}))
   },
   setState: (key, value) => {
+    data.state[key] = value
     // let o = wm.$data.state
     // wm.$set(wm, 'state', Object.assign({}, o, {[key]: value}))
   },
